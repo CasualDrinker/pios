70237a51dfeb8b992e510387463ad35d9ee54f20
CasualDrinker <90201240+CasualDrinker@users.noreply.github.com>
2024-09-16 21:32:24 -0500

    make debug fix

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -4,21 +4,21 @@ char glbl[128];
 
 void kernel_main() {
 	extern int __bss_start, __bss_end;
-	char *bssstart, *bssend;
+	char* bssstart, * bssend;
 
 
 
-	bssstart = (char *) &__bss_start;
-	bssend =(char *) &__bss_end;
-	
+	bssstart = (char*)&__bss_start;
+	bssend = (char*)&__bss_end;
 
-	
 
-	for (char * p = bssstart; p < bssend; ++p )
+
+
+	for (char* p = bssstart; p < bssend; ++p)
 	{
-	
-			*p=0;	
-			
+
+		*p = 0;
+
 	}
 
 
