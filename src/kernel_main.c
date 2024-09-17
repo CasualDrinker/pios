dcec0be0cf55d8afe96aa6270c93c75685f4bae5
CasualDrinker <90201240+CasualDrinker@users.noreply.github.com>
2024-09-17 17:01:28 -0500

    bss clear to zero comeplete

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,24 @@
 
-
+char glbl[128];
 
 void kernel_main() {
+	
+	extern char  __bss_start,  __bss_end;
 
-    while(1){
-    }
+   
+
+	char *bssstart = &__bss_start;
+	char *bssend = &__bss_end;
+	
+	for (char *p = bssstart; p < bssend; p++)
+	{
+	/*	*p = 0xFF;  //test case.*/ // run multiple times in make debug
+		*p = 0;
+	
+	}
+
+
+ 	
+    
 }
+
